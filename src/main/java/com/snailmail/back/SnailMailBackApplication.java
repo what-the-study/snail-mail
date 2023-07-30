2ffe217af00b0f84ab39b22284e6e787683258db
kylekim2123 <kyle2123@naver.com>
2023-07-30 13:52:09 +0900

    build: 프로젝트 초기 환경 설정

---
+++ src/main/java/com/snailmail/back/SnailMailBackApplication.java
@@ -0,0 +1,13 @@
+package com.snailmail.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnailMailBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnailMailBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/snailmail/back/SnailMailBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snailmail.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnailMailBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
