bb37d0b43f9b46062260944842f25f8c0ac2e645
kylekim2123 <kyle2123@naver.com>
2023-07-30 15:12:36 +0900

    feat(B001): Letter 엔티티 컬럼 설정

---
+++ src/main/java/com/snailmail/back/domain/Letter.java
@@ -0,0 +1,76 @@
+package com.snailmail.back.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "letters")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Letter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String reservationKey;
+
+    @Column(nullable = false, length = 15)
+    private String senderName;
+
+    @Column(nullable = false, length = 15)
+    private String recipientName;
+
+    @Column(nullable = false, length = 50)
+    private String recipientEmail;
+
+    @Column(nullable = false)
+    private LocalDate scheduledDate;
+
+    @Column(nullable = false)
+    private Integer duration;
+
+    @Lob
+    private String content;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    private LetterStatus letterStatus;
+
+    private LocalDateTime createdDatetime;
+    private LocalDateTime updatedDatetime;
+
+    @Builder
+    public Letter(String reservationKey, String senderName, String recipientName,
+            String recipientEmail, LocalDate scheduledDate, Integer duration,
+            String content, String password, LetterStatus letterStatus, LocalDateTime createdDatetime,
+            LocalDateTime updatedDatetime) {
+        this.reservationKey = reservationKey;
+        this.senderName = senderName;
+        this.recipientName = recipientName;
+        this.recipientEmail = recipientEmail;
+        this.scheduledDate = scheduledDate;
+        this.duration = duration;
+        this.content = content;
+        this.password = password;
+        this.letterStatus = letterStatus;
+        this.createdDatetime = createdDatetime;
+        this.updatedDatetime = updatedDatetime;
+    }
+}
---
+++ src/main/java/com/snailmail/back/domain/LetterStatus.java
@@ -0,0 +1,8 @@
+package com.snailmail.back.domain;
+
+public enum LetterStatus {
+
+    SCHEDULED,
+    SENT,
+    FAILURE
+}
